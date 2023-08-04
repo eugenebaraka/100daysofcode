2236b3d70aeb975a2b962f359d58d7008b50ad07
Eugene Baraka <eugenebaraka@gmail.com>
2023-08-03 21:30:35 -0400

    complete the quiz game project

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -5,12 +5,18 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question_answer_ in question_data:
-    question = question_answer_["text"]
-    answer = question_answer_["answer"]
+    question = question_answer_["question"]
+    answer = question_answer_["correct_answer"]
     new_question = Question(question=question, answer=answer)
 
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.new_question()
+
+while quiz.still_has_question():
+    quiz.new_question()
+
+
+
+
 
--- Day 17/quiz-game-start/quiz_brain.py
+++ Day 17/quiz-game-start/quiz_brain.py
@@ -2,9 +2,30 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_list = question_list
         self.question_number = 0
+        self.points = 0
+
+    def still_has_question(self):
+        n_questions = len(self.question_list)
+        if n_questions >= self.question_number + 1:
+            return True
+        else:
+            print(f"You've completed the quiz! Your final score is {self.points}/{n_questions}")
+
 
     def new_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        prompt = input(f"Q.{self.question_number}: {current_question.question} (True/False)")
-        return prompt
+        user_answer = input(f"Q.{self.question_number}: {current_question.question} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.points += 1
+            print(f"Your current score is: {self.points}/{self.question_number}")
+        else:
+            print(f"Your current score is: {self.points}/{self.question_number}")
+        print(f"Correct answer: {correct_answer}\n")
+
+
+
+
