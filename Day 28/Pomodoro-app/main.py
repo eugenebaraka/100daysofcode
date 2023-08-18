085d4b7fc06d9bcb2e470820f923bfcbffdec568
Eugene Baraka <eugenebaraka@gmail.com>
2023-08-17 22:40:43 -0400

    continue pomodoro app

--- Day 28/Pomodoro-app/main.py
+++ Day 28/Pomodoro-app/main.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,9 +9,10 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 3
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 2
+SESSION_NUMBER = 1
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -24,29 +26,60 @@ window.title("Pomodoro app")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 # create timer Label at the top
-timer_label = Label(text="Timer", fg=GREEN)
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
+def start_timer():
 
-def start_timer(minutes=25, seconds=0):
-    """Starts the timer when start button is clicked"""
+    global SESSION_NUMBER
 
-    pomodoro_duration = datetime.timedelta(minutes=minutes, seconds=seconds)
-    total_seconds = pomodoro_duration.total_seconds()
+    # work session
+    work_sec = WORK_MIN * 60
 
-    while total_seconds > 0:
-        # keep track of the timer
-        # timer = datetime.timedelta(seconds=total_seconds)
+    # short break
+    short_break = SHORT_BREAK_MIN * 60
 
-        # sleep the program by one second
-        time.sleep(1)
+    # long break
+    long_break = LONG_BREAK_MIN * 60
 
-        # update timer in the pomodoro
-        # canvas["text"] = str(timer)
-        canvas.itemconfig(text_id, text="i am updated")
-        # canvas.grid(column=1, row=1)
-        # reduce the total seconds by one second
-        total_seconds -= 1
+    if SESSION_NUMBER % 2 == 0:
+        if SESSION_NUMBER % 8 == 0:  # every 8th session should be a long break
+            count_down(long_break)
+            print(f"starting session {SESSION_NUMBER}: {LONG_BREAK_MIN}")
+        else:
+            # if it's not an 8th session and is even, it's a short break
+            count_down(short_break)
+            print(f"starting session {SESSION_NUMBER}: {SHORT_BREAK_MIN}")
+    else:
+        # if it's odd, that means it is a work session
+        count_down(work_sec)
+        print(f"starting session {SESSION_NUMBER}: {WORK_MIN}")
+
+    # update session number
+    SESSION_NUMBER += 1
+
+
+def count_down(pomodoro_duration):
+    """Update timer after every second and display on pomodoro canvas.
+
+    args:
+        pomodoro_duration: time in seconds for the session.
+    """
+    count_min = math.floor(pomodoro_duration / 60)
+    count_sec = pomodoro_duration % 60
+
+    if count_min < 10:
+        count_min = "0" + str(count_min) # prepend a zero on minutes if less than 10
+
+    if count_sec < 10:
+        count_sec = "0" + str(count_sec) # prepend a zero on seconds if less than 10
+
+    # update timer on canvas after each second
+    canvas.itemconfig(canvas_text_id, text=f"{count_min}:{count_sec}")
+
+    if pomodoro_duration > 0:
+        # keep track of the timer and update after 1000 milliseconds (1 second)
+        window.after(1000, count_down, pomodoro_duration - 1)
 
 
 # create start and reset buttons
@@ -55,7 +88,7 @@ start_button.grid(column=0, row=2)
 
 
 def reset_timer():
-    pass
+    canvas.itemconfig(canvas_text_id, text="00:00")
 
 
 reset_button = Button(text="Reset", bg=PINK, command=reset_timer)
@@ -68,16 +101,18 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 
 # create text to display time
-text_id = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas_text_id = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-
 # add checkmark to count number of pomodoro sessions done
-checkmark = Label(text="✔", fg=PINK)
+checkmark = Label(text="✔", fg=PINK, bg=YELLOW)
 checkmark.grid(column=1, row=3)
 
 
-
+# TODO #1: Update checkmarks after each pomodoro session to track the number
+# TODO #2: work on the reset session button
+# TODO #3: Allow user to choose number of sessions (maybe later)
+# TODO #4: Add ringtone when time is off
 
 
 window.mainloop()
